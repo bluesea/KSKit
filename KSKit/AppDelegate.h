057ffce54d19fa0d48b443394d8b4e8fb30ddabb
Ping <pingyandong0510@126.com>
2016-11-28 14:43:46 +0800

    Initial Commit

---
+++ KSKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KSKit
+//
+//  Created by pingyandong on 2016/11/28.
+//  Copyright © 2016年 Kingsum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KSKit/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  KSKit
+//
+//  Created by pingyandong on 2016/11/28.
+//  Copyright © 2016年 Kingsum. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
